90f4a11415a89fd122758a639eb3b656528832c8
Kixs <emp_kixs@163.com>
2019-03-21 17:28:32 +0800

    other

---
+++ src/main/java/com/kixs/other/OtherTest.java
@@ -0,0 +1,19 @@
+package com.kixs.other;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author wangbing
+ * @version 1.0
+ * @date 2019/3/21 11:12
+ * Copyright: Copyright (c) 2019
+ */
+public class OtherTest {
+
+    public static void main(String[] args) {
+        PriorityQueue queue = new PriorityQueue();
+
+        String asd = "abd";
+        asd.hashCode();
+    }
+}
