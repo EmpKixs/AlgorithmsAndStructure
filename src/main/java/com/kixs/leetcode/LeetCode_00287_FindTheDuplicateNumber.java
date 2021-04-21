0d8807585fd8698954f4f06e1f07db24657b2915
kixs <emp_kixs@163.com>
2021-04-21 18:28:09 +0800

    leetcode 287

--- src/main/java/com/kixs/leetcode/LeetCode_00287_FindTheDuplicateNumber.java
+++ src/main/java/com/kixs/leetcode/LeetCode_00287_FindTheDuplicateNumber.java
@@ -1,5 +1,8 @@
 package com.kixs.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * url:https://leetcode-cn.com/problems/find-the-duplicate-number/
  * <p>
@@ -15,7 +18,14 @@ public class LeetCode_00287_FindTheDuplicateNumber {
 
 
     public int findDuplicate(int[] nums) {
-        return nums[0];
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
