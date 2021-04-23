21828b6500b3d42f63106900c718e6bd955c8384
kixs <emp_kixs@163.com>
2021-04-23 15:24:51 +0800

    leetcode 34 优化

--- src/main/java/com/kixs/leetcode/LeetCode_00034_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/kixs/leetcode/LeetCode_00034_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -16,52 +16,58 @@ public class LeetCode_00034_FindFirstAndLastPositionOfElementInSortedArray {
 
     public int[] searchRange(int[] nums, int target) {
         int length = nums.length;
-        if (length == 0) {
-            return new int[]{-1, -1};
-        }
-
-        int low = 0;
-        int high = length - 1;
-        int position = -1;
-        if (nums[low] == target) {
-            position = low;
-        }
-        if (position == -1 && nums[high] == target) {
-            position = high;
-        }
-
-        while (position == -1 && low < high) {
-            int tmp = (high + low) / 2;
-            if (nums[tmp] == target) {
-                position = tmp;
-            } else {
-                if (nums[tmp] > target) {
-                    high = tmp - 1;
-                } else {
-                    low = tmp + 1;
+        int left = -1;
+        int right = -1;
+        if (length > 0) {
+            int position = binarySearch(nums, target);
+            if (position > -1) {
+                left = position;
+                right = position;
+                while (left - 1 > 0 && nums[left - 1] == target) {
+                    left--;
+                }
+                while (right + 1 < length && nums[right + 1] == target) {
+                    right++;
                 }
             }
         }
-        if (position == -1) {
-            return new int[]{-1, -1};
+        return new int[]{left, right};
+    }
+
+    private int binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int position = -1;
+        if (nums[low] <= target) {
+            if (nums[low] == target) {
+                position = low;
+            }
+            if (position == -1 && nums[high] >= target) {
+                if (nums[high] == target) {
+                    position = high;
+                }
+                while (position == -1 && low <= high) {
+                    int tmp = (high + low) / 2;
+                    if (nums[tmp] == target) {
+                        position = tmp;
+                    } else {
+                        if (nums[tmp] > target) {
+                            high = tmp - 1;
+                        } else {
+                            low = tmp + 1;
+                        }
+                    }
+                }
+            }
         }
 
-        int left = position - 1;
-        int right = position + 1;
-        while (left > 0 && nums[left] == target) {
-            left--;
-        }
-        while (right < length && nums[right] == target) {
-            right++;
-        }
-
-        return new int[]{left + 1, right - 1};
+        return position;
     }
 
     public static void main(String[] args) {
 
-        int[] nums = {0,1,2,3,4,4,4};
-        int target = 2;
+        int[] nums = {0, 1, 2, 3, 4, 4, 4};
+        int target = 5;
         int[] res = new LeetCode_00034_FindFirstAndLastPositionOfElementInSortedArray().searchRange(nums, target);
         System.out.println(res[0] + "," + res[1]);
     }
