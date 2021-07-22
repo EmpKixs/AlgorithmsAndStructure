9a1d70bb84b6387a9a6bed9b92315c0a69dca850
kixs <emp_kixs@163.com>
2021-07-22 10:26:50 +0800

    ReentrantLockTest

--- src/main/java/com/kixs/other/sync/ReentrantLockTest.java
+++ src/main/java/com/kixs/other/sync/ReentrantLockTest.java
@@ -1,7 +1,10 @@
 package com.kixs.other.sync;
 
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -16,7 +19,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ReentrantLockTest {
 
-    private final static ThreadPoolExecutor POOL = new ThreadPoolExecutor(
+    private static ThreadPoolExecutor POOL = new ThreadPoolExecutor(
             Runtime.getRuntime().availableProcessors(),
             Runtime.getRuntime().availableProcessors() * 2,
             30,
@@ -26,12 +29,83 @@ public class ReentrantLockTest {
             new ThreadPoolExecutor.CallerRunsPolicy());
 
     public static void main(String[] args) {
-        ReentrantLock lock = new ReentrantLock();
-        lock.lock();
-        try {
+        int limit = 10;
+        for (int i = 0; i < limit; i++) {
+            // 无锁操作自增结果 <--> 加锁操作自增结果
+            System.out.println(incrTest(300).getSeed() + " <---> " + incrLockTest(300).getSeed());
+        }
+        POOL.shutdown();
+        POOL = null;
+    }
 
-        } finally {
-            lock.unlock();
+    /**
+     * 无锁自增操作
+     *
+     * @param limit 循环自增次数
+     * @return 自增结果
+     */
+    public static Seed incrTest(int limit) {
+        Seed seed = new Seed(1);
+        List<CompletableFuture<Void>> futures = Lists.newArrayList();
+        for (int i = 0; i < limit; i++) {
+            futures.add(CompletableFuture.runAsync(seed::incrTen, POOL));
+        }
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[]{}));
+        allOf.join();
+        return seed;
+    }
+
+    /**
+     * 加锁自增操作
+     *
+     * @param limit 循环自增次数
+     * @return 自增结果
+     */
+    public static Seed incrLockTest(int limit) {
+        Seed seed = new Seed(1);
+        List<CompletableFuture<Void>> futures = Lists.newArrayList();
+        for (int i = 0; i < limit; i++) {
+            futures.add(CompletableFuture.runAsync(seed::incrLockTen, POOL));
+        }
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[]{}));
+        allOf.join();
+
+        return seed;
+    }
+
+    static class Seed {
+        static int TEN = 10;
+        ReentrantLock lock = new ReentrantLock();
+        int seed = 0;
+
+        Seed(int seed) {
+            this.seed = seed;
+        }
+
+        void incrTen() {
+            for (int i = 0; i < TEN; i++) {
+                seed++;
+            }
+        }
+
+        void incrLockTen() {
+            lock.lock();
+            try {
+                while (!lock.isLocked()) {
+                    lock.lock();
+                }
+                for (int i = 0; i < TEN; i++) {
+                    seed++;
+                }
+            } finally {
+                if (lock.isLocked()) {
+                    lock.unlock();
+                }
+            }
+        }
+
+        int getSeed() {
+            return seed;
         }
     }
 }
